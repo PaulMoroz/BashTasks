e08bea9ea6d81c2970c542e915a0c318475f3dc1
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-08-20 15:52:27 +0300

    Added task

--- first.sh
+++ first.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Self-reproducing Script
+#Write a script that backs itself up, that is, copies itself to a file named backup.sh.
 name="backup"
 while [[ -e $name".sh" ]];do
     name=$name"(1)"
